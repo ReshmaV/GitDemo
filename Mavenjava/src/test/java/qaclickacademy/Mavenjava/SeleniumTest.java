6634d2157b241ab546ef4637d818cbace60735ed
reshma <resh93.murthy@gmail.com>
2021-08-30 00:36:38 +0530

    India-Bharat addition

--- Mavenjava/src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
+++ Mavenjava/src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -8,6 +8,7 @@ public class SeleniumTest {
     public void test3()
     {
         System.out.println( "Hello India!" );
+        System.out.println( "Hello Bharat!" );
     }
     @Test
     public void test4()
